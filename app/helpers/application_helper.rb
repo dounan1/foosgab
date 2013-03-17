0ede7a5b5441040a6cfbeb2902fb3e68f95cf698
Paul Friedman <paulrfri@gmail.com>
2013-03-17 16:58:14 -0400

    Layout changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def nav_link(text, path)
+    css = current_page?(path) ? 'active' : ''
+
+    content_tag(:li, class: css) do
+      link_to text, path
+    end
+  end
+
 end
