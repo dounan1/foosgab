917cf501f4652dc842eeccc3bf8f456d5ab7f9c6
Paul Friedman <paulrfri@gmail.com>
2013-04-13 00:40:15 -0400

    Pluralize!

--- app/models/player.rb
+++ app/models/player.rb
@@ -13,6 +13,7 @@ class Player
     # then scope the rest of win/loss/etc as well
     Game.or({ "red.player_id" => _id }, { "red.offense_id" => _id }, { "red.defense_id" => _id },
       { "blue.player_id" => _id }, { "blue.offense_id" => _id }, { "blue.defense_id" => _id })
+    # Game.or(Hash[["red.player_id", "red.offense_id", "red.defense_id", "blue.player_id", "blue.offense_id", "blue.defense_id"].map {|k| [k,_id]}])
   end
   
   def games_won
@@ -20,7 +21,7 @@ class Player
   end
   
   def games_lost
-    games.select { |g| g.winner and g.winner.players.include? self }
+    games.select { |g| g.loser and g.loser.players.include? self }
   end
   
   def games_tied
