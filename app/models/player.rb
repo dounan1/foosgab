7f90a15ed9c34472969c6bbb944c1422d7a7eebe
Paul Friedman <paulrfri@gmail.com>
2013-04-13 00:18:12 -0400

    Start moving stats queries to Mongoid criteria

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,24 +4,39 @@ class Player
   field :name, type: String
   slug :name
 
+  def percentage
+    ((wins + 0.5 * ties) / (wins + losses + ties)) * 100
+  end
+
   def games
-    Game.all.select { |g| g.players.include? self }.entries
+    # move this to a game scope
+    # then scope the rest of win/loss/etc as well
+    Game.or({ "red.player_id" => _id }, { "red.offense_id" => _id }, { "red.defense_id" => _id },
+      { "blue.player_id" => _id }, { "blue.offense_id" => _id }, { "blue.defense_id" => _id })
+  end
+  
+  def games_won
+    games.select { |g| g.winner and g.winner.players.include? self }
+  end
+  
+  def games_lost
+    games.select { |g| g.winner and g.winner.players.include? self }
+  end
+  
+  def games_tied
+    games.select { |g| g.tie? } 
   end
 
   def wins
-    Game.all.select { |g| g.players.include? self }.select { |g| g.winner and g.winner.players.include? self }.count
+    games_won.count
   end
 
   def losses
-    Game.all.select { |g| g.players.include? self }.select { |g| g.loser and g.loser.players.include? self }.count
+    games_lost.count
   end
 
   def ties
-    Game.all.select { |g| g.players.include? self }.select { |g| g.tie? }.count
-  end
-
-  def percentage
-    ((wins + 0.5 * ties) / (wins + losses + ties)) * 100
+    games_tied.count
   end
 
 end
