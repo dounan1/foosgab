be6e49d17185f227749ef17e90b2ff6e576dec5f
Paul Friedman <paulrfri@gmail.com>
2013-04-29 14:46:04 -0400

    Single quotes because I can

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,93 +1,93 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Player do
-  context "validates" do
-    it "presence of name" do
+  context 'validates' do
+    it 'presence of name' do
       expect(Fabricate.build(:player, name: nil)).to_not be_valid
     end
 
-    it "presence of type" do
+    it 'presence of type' do
       expect(Fabricate.build(:player, type: nil)).to_not be_valid
     end
   end
 
-  context "games helpers" do
+  context 'games helpers' do
     let(:player1) { Fabricate(:player) }
 
-    context "#games" do
-      it "returns empty when player has no games" do
+    context '#games' do
+      it 'returns empty when player has no games' do
         expect(player1.games.entries).to be_empty
       end
 
-      it "returns a list of games when player has games" do
+      it 'returns a list of games when player has games' do
         Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1))
         expect(player1.games.entries).to_not be_empty
       end
     end
 
-    context "for games won" do
+    context 'for games won' do
       before(:each) do
         3.times { Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 10),
           blue: Fabricate.build(:solo, score: 0)) }
       end
 
-      it "returns the complete list of games won" do
+      it 'returns the complete list of games won' do
         expect(player1.games_won.entries).to have(3).items
       end
 
-      it "returns the number of games won" do
+      it 'returns the number of games won' do
         expect(player1.wins).to eq 3
       end
     end
 
-    context "for games lost" do
+    context 'for games lost' do
       before(:each) do
         3.times { Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 0),
           blue: Fabricate.build(:solo, score: 10)) }
       end
 
-      it "returns the complete list of games lost" do
+      it 'returns the complete list of games lost' do
         expect(player1.games_lost.entries).to have(3).items
       end
 
-      it "returns the number of games lost" do
+      it 'returns the number of games lost' do
         expect(player1.losses).to eq 3
       end
     end
 
-    context "for games tied" do
+    context 'for games tied' do
       before(:each) do
         3.times { Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 5),
           blue: Fabricate.build(:solo, score: 5)) }
       end
 
-      it "returns the complete list of games tied" do
+      it 'returns the complete list of games tied' do
         expect(player1.games_tied.entries).to have(3).items
       end
 
-      it "returns the number of games tied" do
+      it 'returns the number of games tied' do
         expect(player1.ties).to eq 3
       end
     end
   end
 
-  context "stats helpers" do
+  context 'stats helpers' do
     let(:player1) { Fabricate(:player) }
 
-    context "#average_score" do
-      it "returns the average score" do
+    context '#average_score' do
+      it 'returns the average score' do
         Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 6))
         Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 4))
         expect(player1.average_score).to eq 5
       end
 
-      it "returns 0 when player has no games" do
+      it 'returns 0 when player has no games' do
         expect(player1.average_score).to eq 0
       end
     end
 
-    context "#win_pct" do
-      it "returns the winning percentage" do
+    context '#win_pct' do
+      it 'returns the winning percentage' do
         Fabricate(:solo_game, red: Fabricate.build(:solo, score: 10, player: player1),
           blue: Fabricate.build(:solo, score: 1))
 
@@ -99,7 +99,7 @@ describe Player do
         expect(player1.win_pct).to eq 0.25
       end
 
-      it "returns 0 when player has no games" do
+      it 'returns 0 when player has no games' do
         expect(player1.average_score).to eq 0
       end
     end
