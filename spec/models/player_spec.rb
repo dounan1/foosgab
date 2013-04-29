f5fdf7f9c88c390df764757275cdd328baabbefd
Paul Friedman <paulrfri@gmail.com>
2013-04-29 14:38:28 -0400

    Refactor some player tests

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -11,50 +11,96 @@ describe Player do
     end
   end
 
-  context "#games" do
-    it "returns empty when player has no games" do
-      expect(Fabricate(:player).games.entries).to be_empty
+  context "games helpers" do
+    let(:player1) { Fabricate(:player) }
+
+    context "#games" do
+      it "returns empty when player has no games" do
+        expect(player1.games.entries).to be_empty
+      end
+
+      it "returns a list of games when player has games" do
+        Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1))
+        expect(player1.games.entries).to_not be_empty
+      end
     end
 
-    it "returns a list of games when player has games" do
-      player = Fabricate(:player)
-      game = Fabricate(:solo_game, red: Fabricate.build(:solo, player: player))
+    context "for games won" do
+      before(:each) do
+        3.times { Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 10),
+          blue: Fabricate.build(:solo, score: 0)) }
+      end
 
-      expect(player.games.entries).to_not be_empty
+      it "returns the complete list of games won" do
+        expect(player1.games_won.entries).to have(3).items
+      end
+
+      it "returns the number of games won" do
+        expect(player1.wins).to eq 3
+      end
+    end
+
+    context "for games lost" do
+      before(:each) do
+        3.times { Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 0),
+          blue: Fabricate.build(:solo, score: 10)) }
+      end
+
+      it "returns the complete list of games lost" do
+        expect(player1.games_lost.entries).to have(3).items
+      end
+
+      it "returns the number of games lost" do
+        expect(player1.losses).to eq 3
+      end
+    end
+
+    context "for games tied" do
+      before(:each) do
+        3.times { Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 5),
+          blue: Fabricate.build(:solo, score: 5)) }
+      end
+
+      it "returns the complete list of games tied" do
+        expect(player1.games_tied.entries).to have(3).items
+      end
+
+      it "returns the number of games tied" do
+        expect(player1.ties).to eq 3
+      end
     end
   end
 
   context "stats helpers" do
+    let(:player1) { Fabricate(:player) }
+
     context "#average_score" do
       it "returns the average score" do
-        player = Fabricate(:player)
-        Fabricate(:solo_game, red: Fabricate.build(:solo, player: player, score: 6))
-        Fabricate(:solo_game, red: Fabricate.build(:solo, player: player, score: 4))
-        expect(player.average_score).to eq 5
+        Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 6))
+        Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1, score: 4))
+        expect(player1.average_score).to eq 5
       end
 
       it "returns 0 when player has no games" do
-        expect(Fabricate(:player).average_score).to eq 0
+        expect(player1.average_score).to eq 0
       end
     end
 
     context "#win_pct" do
       it "returns the winning percentage" do
-        player = Fabricate(:player)
-
-        Fabricate(:solo_game, red: Fabricate.build(:solo, score: 10, player: player),
+        Fabricate(:solo_game, red: Fabricate.build(:solo, score: 10, player: player1),
           blue: Fabricate.build(:solo, score: 1))
 
-        expect(player.win_pct).to eq 1
+        expect(player1.win_pct).to eq 1
 
-        3.times { Fabricate(:solo_game, red: Fabricate.build(:solo, score: 1, player: player),
+        3.times { Fabricate(:solo_game, red: Fabricate.build(:solo, score: 1, player: player1),
                                 blue: Fabricate.build(:solo, score: 10)) }
 
-        expect(player.win_pct).to eq 0.25
+        expect(player1.win_pct).to eq 0.25
       end
 
       it "returns 0 when player has no games" do
-        expect(Fabricate(:player).average_score).to eq 0
+        expect(player1.average_score).to eq 0
       end
     end
   end
