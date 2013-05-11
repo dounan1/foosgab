e40d759d80b138536d57dbd28f79000b7832bb33
Paul Friedman <paulrfri@gmail.com>
2013-05-11 11:41:26 -0400

    Most common partner/opponent tests

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe Player do
   let(:player1) { Fabricate(:player) }
+  let(:player2) { Fabricate(:player) }
+  let(:player3) { Fabricate(:player) }
 
   it 'has a valid factory' do
     expect(player1).to be_valid
@@ -103,6 +105,44 @@ describe Player do
 
       it 'returns 0 when player has no games' do
         expect(player1.average_score).to eq 0
+      end
+    end
+
+    context '#most_common_partner' do
+      it 'returns the most common partner' do
+        Fabricate(:game, red: Fabricate.build(:team, offense: player1, defense: player2))
+        expect(player1.most_common_partner).to eq [player2]
+      end
+
+      it 'returns multiple partners when both are most common' do
+        Fabricate(:game, red: Fabricate.build(:team, offense: player1, defense: player2))
+        Fabricate(:game, red: Fabricate.build(:team, offense: player1, defense: player3))
+        expect(player1.most_common_partner).to eq [player2, player3]
+      end
+
+      it 'returns nothing when player has no team games' do
+        Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1))
+        expect(player1.most_common_partner).to be_empty
+      end
+    end
+
+    context '#most_common_opponent' do
+      it 'returns the most common opponent' do
+        Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1), 
+          blue: Fabricate.build(:solo, player: player2))
+        expect(player1.most_common_opponent).to eq [player2]
+      end
+
+      it 'returns multiple opponents when both are most common' do
+        Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1), 
+          blue: Fabricate.build(:solo, player: player2))
+        Fabricate(:solo_game, red: Fabricate.build(:solo, player: player1), 
+          blue: Fabricate.build(:solo, player: player3))
+        expect(player1.most_common_opponent).to eq [player2, player3]
+      end
+
+      it 'returns nothing when player has no games' do
+        expect(player1.most_common_opponent).to be_empty
       end
     end
   end
